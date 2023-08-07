f0d7e52406d7ae8b71f7242c490e68afae9148d7
Gabriel J. Nunes <33209660+gabejnunes@users.noreply.github.com>
2023-08-07 12:50:11 -0300

    Add files via upload

---
+++ _next/static/chunks/app/page-bb20837ad799815b.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{570:function(e,r,t){Promise.resolve().then(t.bind(t,4976))},4976:function(e,r,t){"use strict";t.r(r);var n=t(7437);r.default=()=>(0,n.jsx)("div",{className:"h-full flex justify-center m-8",children:(0,n.jsx)("div",{className:"rounded-lg bg-slate-100 h-[28rem] w-96"})})},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function l(e,r,t){var n,l={},c=null,i=null;for(n in void 0!==t&&(c=""+t),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(i=r.ref),r)s.call(r,n)&&!u.hasOwnProperty(n)&&(l[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===l[n]&&(l[n]=r[n]);return{$$typeof:o,type:e,key:c,ref:i,props:l,_owner:f.current}}r.jsx=l,r.jsxs=l},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,596,744],function(){return e(e.s=570)}),_N_E=e.O()}]);
